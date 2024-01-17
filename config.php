9127128ff0d7b39c1dd689145e2d2047941f13ec
Poom <120092842+Poom156@users.noreply.github.com>
2024-01-17 14:38:29 +0700

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host: "us-cluster-east-01.k8s.cleardb.net"
-$db_user: "bdf27f4bfc0305"
-$db_pass: "932c3912"
-$db_name: "heroku_37ed812309da6b8"
-
-$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
