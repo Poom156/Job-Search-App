71ce82c34dc0f558b2417dbff96ccc35904bd63c
unknown <poom.pupat156@gmail.com>
2024-01-17 13:44:17 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require __DIR__ . '/../vendor/autoload.php';
+
+use Framework\Router;
+use Framework\Session;
+
+Session::start();
+
+require '../helpers.php';
+//Instantiate router
+$router = new Router();
+
+//Get routes
+$routes = require basePath('routes.php');
+
+//Get current URI and HTTP method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+//Route the request
+$router->route($uri);
